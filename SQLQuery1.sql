35da20ba47499938bbbf18630cd0760ee8739087
Eshwar Inamdar <eshwarinamdar51@gmail.com>
2024-09-16 14:57:17 +0530

    .NET TINY : ORM-PROJECT

---
+++ SQLQuery1.sql
@@ -0,0 +1,29 @@
+﻿select * from users;
+select * from Account;
+
+sp_help Account;
+sp_help users;
+
+-- Create User table
+CREATE TABLE users (
+    userid INT PRIMARY KEY identity(1,1),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+-- Create Account table
+CREATE TABLE Account (
+    accNumber INT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(20),
+    balance FLOAT,
+    userid INT,
+    FOREIGN KEY (userid) REFERENCES users(userid)  on delete cascade,
+);
+
+drop table Account;
+drop table users;
+
+CREATE TABLE Account(accNumber INT     ,name VARCHAR(100)     ,email VARCHAR(100)     ,phone VARCHAR(100)     ,balance FLOAT     ,userid INT FOREIGN KEY REFERENCES  users (userid) ON DELETE CASCADE);
+CREATE TABLE users(userid INT PRIMARY KEY IDENTITY(1,1)   ,email VARCHAR(100)     ,password VARCHAR(100)     );
---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿SELECT a.name FROM users u,Account a WHERE u.email = eshwar@gmail.com AND u.password =123 AND u.userid = a.userid;
+
+select * from users;
+select * from Account;
+
+SELECT a.name FROM users u JOIN Account a ON u.userid = a.userid WHERE u.email = 'eshwar@gmail.com' AND u.password = '123';
